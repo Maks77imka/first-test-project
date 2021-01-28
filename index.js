ffae6250d95f81a71035218e81585c1fb724d3d9
TheStitch <torchlight15@gmail.com>
2021-01-28 21:29:44 +0300

    Added some comments to JS file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// Get acces to form and text field where we are going to post answers.
 const form = document.querySelector(".bot_forms form");
 const botAnswer = document.querySelector("#answer");
 const typeAnimation = "animate__bounceIn";
-
+// Create data base object with key: value of answers.
 let db = {
   "Что ты умеешь?": "На самом деле, немного, я - ранняя тестовая версия искуственного интелекта.\
   Посмотрите мой исходный код, чтобы узнать список вопросов, на которые я могу ответить",
@@ -11,11 +12,14 @@ let db = {
   "I'm fine": "That's great!"
 }
 
+// Create function that delete our animation class.
 function handleAnimationEnd() {
   botAnswer.classList.remove(typeAnimation);
 }
 
+// Create event for answering when we click "submit".
 form.addEventListener("submit", (event) => {
+  // preventDefault() delete standart page-reload when we "submit".
   event.preventDefault();
   const value = event.target.bottext.value;
 
@@ -27,8 +31,9 @@ form.addEventListener("submit", (event) => {
     } else {
       botAnswer.innerHTML = "I don't know it, Sir"
     }
-
+    // add class with animation, when we get answer.
     botAnswer.classList.add(typeAnimation);
-    botAnswer.addEventListener("animationend", handleAnimationEnd, {once: true});
+    // delete previos class with animation.
+    botAnswer.addEventListener("animationend", handleAnimationEnd, { once: true });
   }
 })
